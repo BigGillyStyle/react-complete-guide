2329bdc11c7d55beb36a0eed1f717967e47f38b7
Andy Pickler <andy.pickler@gmail.com>
2022-01-19 18:20:52 -0700

    Step 37

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expense = {
-    date: new Date(2021, 2, 28),
-    title: "Car Insurance",
-    amount: 294.67,
-  };
+function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <div>{expense.date.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
-        <div className="expense-item__price">${expense.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
