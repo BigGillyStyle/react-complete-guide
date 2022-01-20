7ca22820fe39e1d812dfe8c7a6afd7af0fdd007e
Andy Pickler <andy.pickler@gmail.com>
2022-01-19 17:30:15 -0700

    Step 36

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expense = {
+    date: new Date(2021, 2, 28),
+    title: "Car Insurance",
+    amount: 294.67,
+  };
+  return (
+    <div className="expense-item">
+      <div>{expense.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
